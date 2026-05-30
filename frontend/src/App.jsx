6ee8808cd84e6e63a7163acf4e2ee9df1f2a15d8
Srijeet Biswas <srijeetbiswas.sb@gmail.com>
2026-05-30 10:08:04 +0530

    Updated API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import {
   useNavigate,
 } from "react-router-dom";
 import "./App.css";
-const API_BASE_URL = API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function HomePage() {
   const [batches, setBatches] = useState([]);
